1c72f7fd07a29c67ca0fb4f2084cc28b067aebdf
Ivan Grekov <ivan.grekov@windmill.digital>
2024-10-15 15:18:09 +0300

    fix redirect on sign out

--- src/components/sign-out-button/SignOutButton.tsx
+++ src/components/sign-out-button/SignOutButton.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
 
-import { useRouter } from 'next/navigation';
-
 import { setCookie } from 'actions/setCookie';
 import Button from 'components/button/Button';
 import { SESSION_COOKIE_NAME } from 'constants/cookiesKeys.constants';
 import { EAppRoutes } from 'types/appRoutes';
 
 export default function SignOutButton(): JSX.Element {
-    const router = useRouter();
     const [isLoading, setIsLoading] = useState(false);
 
     const onClick = async (): Promise<void> => {
@@ -18,7 +15,7 @@ export default function SignOutButton(): JSX.Element {
             key: SESSION_COOKIE_NAME,
         });
 
-        router.replace(EAppRoutes.Auth);
+        window.location.href = EAppRoutes.Auth;
     };
 
     return <Button text="Sign out" onClick={onClick} isLoading={isLoading} />;
