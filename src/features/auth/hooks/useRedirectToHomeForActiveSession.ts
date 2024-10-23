4b34cea0cc7755e22d51a31436fc105ceffe60f1
Ivan Grekov <ivan.grekov@windmill.digital>
2024-10-23 18:56:17 +0300

    refactor useRedirectToHomeForActiveSession hook

--- src/features/auth/hooks/useRedirectToHomeForActiveSession.ts
+++ src/features/auth/hooks/useRedirectToHomeForActiveSession.ts
@@ -1,19 +1,27 @@
 import { useEffect } from 'react';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 
 import { SESSION_COOKIE_NAME } from 'constants/cookiesKeys.constants';
 import { EAppRoutes } from 'types/appRoutes';
 import { getCookie } from 'utils/getCookie';
+import { getIsAuthPage } from 'utils/getIsAuthPage';
 
 export const useRedirectToHomeForActiveSession = (): void => {
     const router = useRouter();
+    const pathname = usePathname();
 
     useEffect(() => {
+        if (!getIsAuthPage(pathname)) {
+            return;
+        }
+
         const sessionCookie = getCookie(SESSION_COOKIE_NAME);
 
-        if (sessionCookie) {
-            router.push(EAppRoutes.Root);
+        if (!sessionCookie) {
+            return;
         }
-    }, [router]);
+
+        window.location.href = EAppRoutes.Root;
+    }, [pathname, router]);
 };
